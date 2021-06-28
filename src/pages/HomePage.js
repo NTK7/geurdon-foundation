94264d35e1380c4b7c843224d336ca79e43d1cf3
Nazhim <nazhimkalam@gmail.com>
2021-06-28 23:08:37 +0530

    added responsive title

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,36 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
 const HomePage = () => {
 	return (
 		<Container>
-			<h2>Homepage</h2>
-            {/* Image Carrossel */}
-            {/* Donote Now button */}
-            {/* who are we */}
-            {/* Our programs */}
+			<h3>Guerdon Foundation</h3>
+			{/* Image Carrossel */}
+			{/* Donote Now button */}
+			{/* who are we */}
+			{/* Our programs */}
 		</Container>
 	);
 };
 
 export default HomePage;
 
-const Container = styled.div``;
+const Container = styled.div`
+	margin: 30px 0;
+	/* border: 1px blue solid; */
+	> h3 {
+		text-align: center;
+		color: #ff534a;
+		display: none;
+	}
+
+	@media screen and (max-width: 900px) {
+		> h3 {
+			display: block;
+		}
+	}
+    @media screen and (max-width: 400px) {
+		> h2 {
+			font-size: medium;
+		}
+	}
+`;
