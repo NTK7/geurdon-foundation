442b3f289e3d955fb97d3487e0847b413674a9dc
Nazhim <nazhimkalam@gmail.com>
2021-06-28 21:55:01 +0530

    removed unused code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import DrawableTab from './shared/DrawableTab/DrawableTab';
 import Footer from './shared/Footer';
