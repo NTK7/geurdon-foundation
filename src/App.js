c1763997722aca9a640aa4fc55c473c195e8088a
Nazhim <nazhimkalam@gmail.com>
2021-07-16 19:28:51 +0530

    site taken down, due to no payments made

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import { useEffect } from 'react';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import HomePage from './pages/HomePage';
-// import DrawableTab from './shared/DrawableTab/DrawableTab';
-// import Footer from './shared/Footer';
-// import Header from './shared/Header';
+import { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import HomePage from "./pages/HomePage";
+// import DrawableTab from "./shared/DrawableTab/DrawableTab";
+import Footer from "./shared/Footer";
+// import Header from "./shared/Header";
 
 const App = () => {
-	useEffect(() => {
-		caches.keys().then((names) => {
-			names.forEach((name) => {
-				caches.delete(name);
-			});
-		});
-	}, []);
+  useEffect(() => {
+    caches.keys().then((names) => {
+      names.forEach((name) => {
+        caches.delete(name);
+      });
+    });
+  }, []);
 
-	return (
-		// <Router>
-			// <div>
-				{/* <Header />
-				<DrawableTab />
-				<Switch>
-					<Route path="/">
-						<HomePage />
-					</Route>
-				</Switch>
-				<Footer /> */}
-				<h1>Site taken down</h1>
-			// </div>
-		// </Router>
-	);
+  return (
+    <Router>
+      <div>
+        {/* <Header /> */}
+        {/* <DrawableTab /> */}
+        <Switch>
+          <Route path="/">
+            {/* <HomePage /> */}
+            <h1>Site taken down</h1>
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 };
 
 export default App;
